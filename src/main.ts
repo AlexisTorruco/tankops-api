14af4e903ea506379c6b2fd4284965cad3a9f268
AlexisTorruco <altorrucoc@hotmail.com>
2026-06-27 00:01:34 -0600

    feat(auth): implement login with JWT

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 
   console.log(`🚀 Server is running`);
 }
